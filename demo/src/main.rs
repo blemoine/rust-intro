33c5a8eec5f22800787ba40f0e4369e696a5f750
Benoit Lemoine <blemoine@mnubo.com>
2019-07-16 08:02:32 -0400

    update

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,24 +1,23 @@
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct User { username: String }
 
 #[derive(Debug)]
-struct CoffeeMachine<'a> {
+struct CoffeeMachine {
     water_tank: u32,
     is_on: bool,
-    owner: &'a User
+    owner: &User
 }
 
-impl <'a> CoffeeMachine<'a> {
-    pub fn new(owner: &'a User) -> CoffeeMachine<'a> {
+impl CoffeeMachine {
+    pub fn new(owner: &User) -> CoffeeMachine {
         CoffeeMachine { owner, water_tank: 0, is_on: false }
     }
 }
 
 fn main() {
-    let user = User { username: "Georges".to_owned() };
-    let coffee_machine = CoffeeMachine::new(&user) ;
-    let coffee_machine2 = CoffeeMachine::new(&user) ;
+let user = User { username: "Georges".to_owned() };
+let coffee_machine = CoffeeMachine::new(&user) ;
+let coffee_machine2 = CoffeeMachine::new(&user) ;
 
-    println!("{:?}", coffee_machine)
+println!("{:?}", coffee_machine)
 }
